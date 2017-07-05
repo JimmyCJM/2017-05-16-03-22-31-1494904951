500bc5fb61ca99c9a460c60fb9f5efe32727f8be
JimmyCJM <chijieming@sina.com>
2017-07-05 17:10:38 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,136 +1,80 @@
 module.exports = function main(input) {
-        var line1='',line2='',line3='';
+        var line1 = new Array(),line2 = new Array(),line3 = new Array();
         var ar=new Array();
-        for(var j=0;j<input.length;j++)
+        for(var i=0;i<input.length;i++)
         {
-	    ar[j]=parseInt(input[j]);
+	    ar[i]=parseInt(input[i]);
         }
     
   for(var i=0;i<input.length;i++)
   {
   switch(ar[i]){
   case 0 :
-      line1 +='._.';
-	  line2 +='|.|';
-	  line3 +='|_|';
-      if(i<(input.length-1))
-      {
-	 line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break; 
+	line1[i] ='._.';
+	line2[i] ='|.|';
+	line3[i] ='|_|';
+	break; 
           
   case 1 : 
-      line1 +='...';
-	  line2 +='..|';
-	  line3 +='..|';
-	 if(i<(input.length-1))
-     {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+	line1[i] ='...';
+	line2[i] ='..|';
+	line3[i] ='..|';
+	break;
           
   case 2 : 
-      line1 +='._.';
-	  line2 +='._|';
-	  line3 +='|_.';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+	line1[i] ='._.';
+	line2[i] ='._|';
+	line3[i] ='|_.';
+	break;
           
   case 3 : 
-      line1 +='._.';
-	  line2 +='._|';
-	  line3 +='._|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+	line1[i] ='._.';
+	line2[i] ='._|';
+	line3[i] ='._|';
+	break;
           
   case 4 : 
-      line1 +='...';
-	  line2 +='|_|';
-	  line3 +='..|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;  
+	line1[i] ='...';
+	line2[i] ='|_|';
+	line3[i] ='..|';
+	break;  
           
-case 5 : 
-      line1 +='._.';
-	  line2 +='|_.';
-	  line3 +='._|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+  case 5 : 
+	line1[i] ='._.';
+	line2[i] ='|_.';
+	line3[i] ='._|';
+	break;
           
   case 6 : 
-      line1 +='._.';
-	  line2 +='|_.';
-	  line3 +='|_|';
-	if(i<(input.length-1)){
-	 line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+	line1[i] ='._.';
+	line2[i] ='|_.';
+	line3[i] ='|_|';
+	break;
           
   case 7 : 
-      line1 +='._.';
-	  line2 +='..|';
-	  line3 +='..|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;
+	line1[i] ='._.';
+	line2[i] ='..|';
+	line3[i] ='..|';
+	break;
           
   case 8 : 
-      line1 +='._.';
-	  line2 +='|_|';
-	  line3 +='|_|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;  
+	line1[i] ='._.';
+	line2[i] ='|_|';
+	line3[i] ='|_|';
+	break;  
           
-	case 9 : 
-      line1 +='._.';
-	  line2 +='|_|';
-	  line3 +='..|';
-	if(i<(input.length-1))
-      {
-	  line1 +=' ';
-	  line2 +=' ';
-	  line3 +=' ';
-	  }
-	  break;  
-   default :
-   break;	  
+  case 9 : 
+	line1[i] ='._.';
+	line2[i] ='|_|';
+	line3[i] ='..|';
+	break;  
+  default :
+  break;	  
   }
   }
+  line1.join(' ');
+  line2.join(' ');
+  line3.join(' ');
   var line=(line1+'\n'+line2+'\n'+line3+'\n')
   return line;
 };
